54298eec4f012fd31f0ad9bc3c18299a1580f24b
aryansharmaportfolio <aryansharmaus2021@gmail.com>
2025-10-13 22:50:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import ProjectDetail from "./pages/ProjectDetail";
-import ClubDetail from "./pages/ClubDetail";
-import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useLocation,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import Index from "@/pages/Index";
+import ClubDetail from "@/pages/ClubDetail";
+import ProjectDetail from "@/pages/ProjectDetail";
+import NotFound from "@/pages/NotFound";
 
-const queryClient = new QueryClient();
+function ScrollToAnchor() {
+  const { pathname, hash } = useLocation();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/project/:id" element={<ProjectDetail />} />
-          <Route path="/club/:id" element={<ClubDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+  useEffect(() => {
+    if (hash) {
+      setTimeout(() => {
+        const element = document.getElementById(hash.substring(1));
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100);
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToAnchor />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/club/:id" element={<ClubDetail />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;

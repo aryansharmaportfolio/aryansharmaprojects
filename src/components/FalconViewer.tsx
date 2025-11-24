2d06183508b8bd3a369e85ca57e4f9f57f4ac02f
aryansharmaportfolio <aryansharmaus2021@gmail.com>
2025-11-24 16:11:49 -0600

    Update FalconViewer.tsx

--- src/components/FalconViewer.tsx
+++ src/components/FalconViewer.tsx
@@ -50,8 +50,8 @@ const ZOOM_ZONES = {
   
   // FIXED: Lifted Y offsets by 120 units to target the black cylinder properly
   interstage: { 
-    offset: [130, 120, 80],      // Lifted Camera UP
-    lookOffset: [1000, 120, 0],      // Lifted Target UP
+    offset: [100, 120, 80],      // Lifted Camera UP
+    lookOffset: [-50, 120, 0],      // Lifted Target UP
     type: "dynamic",
     refId: "middle" 
   },

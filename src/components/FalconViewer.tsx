10442098899f35faa42a1fd1ae365fae73754e0b
aryansharmaportfolio <aryansharmaus2021@gmail.com>
2025-11-27 15:44:16 -0600

    Update FalconViewer.tsx

--- src/components/FalconViewer.tsx
+++ src/components/FalconViewer.tsx
@@ -35,7 +35,7 @@ const ZOOM_ZONES = {
   
   // DYNAMIC PARTS
   fairing: { 
-    offset: [100, 40, -100],     
+    offset: [100, 40, 30],     
     lookOffset: [-100, 20, 0],     
     type: "dynamic",
     refId: "top"    

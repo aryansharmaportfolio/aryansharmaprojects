d61232dfe69e14599ac6095d4593d401a00a1837
aryansharmaportfolio <aryansharmaus2021@gmail.com>
2026-01-06 13:12:45 -0600

    Update ZoomerCFDViewer.tsx

--- src/components/ZoomerCFDViewer.tsx
+++ src/components/ZoomerCFDViewer.tsx
@@ -87,8 +87,8 @@ export default function ZoomerCFDViewer() {
           <CameraControls 
             minPolarAngle={0} 
             maxPolarAngle={Math.PI / 1.8} 
-            minDistance={100} 
-            maxDistance={2000} 
+            minDistance={1200} 
+            maxDistance={1300} 
           />
           <ContactShadows resolution={1024} scale={300} blur={3} opacity={0.2} far={100} color="#000000" />
         </Suspense>
